2ccfa9d5ad99634a8dd9ecfd7b01465d66e43e6d
Batoolkharraz <99608072+Batoolkharraz@users.noreply.github.com>
2023-09-25 21:43:08 +0300

    onboarding page

--- lib/onboarding/introduction.dart
+++ lib/onboarding/introduction.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/Auth/signup.dart';
+import 'package:flutter_application_4/onboarding/intro_screen1.dart';
+import 'package:flutter_application_4/onboarding/intro_screen2.dart';
+import 'package:flutter_application_4/onboarding/intro_screen3.dart';
+import 'package:flutter_application_4/onboarding/intro_screen4.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Introduction extends StatefulWidget {
   const Introduction({Key? key}) : super(key: key);
@@ -8,48 +14,74 @@ class Introduction extends StatefulWidget {
 }
 
 class _IntroductionState extends State<Introduction> {
+  // to keep track of which page we are
   PageController _controller = PageController();
+
+  // keep track if we are at the last page
+  bool onLastPage = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column( // Wrap your PageView and SmoothPageIndicator in a Column
+      body: Stack(
+        // Wrap your PageView and SmoothPageIndicator in a Column
         children: [
-          Expanded(
-            child: PageView(
-              controller: _controller,
+          PageView(
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 3);
+              });
+            },
+            children: [
+              IntroPage1(),
+              IntroPage2(),
+              IntroPage3(),
+              IntroPage4(),
+            ],
+          ),
+          Container(
+            alignment: Alignment(0, 0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                 Container(margin: EdgeInsets.fromLTRB(0,50,10, 0),
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    children: [
-                      Text("Your Heath",
-                      style:
-                      TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 45,
-                        fontFamily: 'FredokOne',
-                        
-                        color:Color.fromARGB(151, 18, 92, 220)
-                      ),),
-                      Image.asset("assets/doctor2.jpg",
+                //skip
+                GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(3);
+                    },
+                    child: Text('skip',
+                        style: TextStyle(fontSize: 27),)),
+
+                //dot indicator
+                SmoothPageIndicator(controller: _controller, count: 4),
+
+                //next or done
+                onLastPage
+                    ? GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) {
+                              return Signup();
+                            }),
+                          );
+                        },
+                        child: Text('done',
+                        style: TextStyle(fontSize: 27),),
+                      )
+                    : GestureDetector(
+                        onTap: () {
+                          _controller.nextPage(
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeIn);
+                        },
+                        child: Text('next',
+                        style: TextStyle(fontSize: 27),),
                       ),
-                      SizedBox(height: 40.0),
-                       ],
-                  )
-                )
-                ,Container(
-                  color: Colors.green,
-                )
-                ,Container(
-                  color: Colors.blue,
-                )
-               
               ],
             ),
           ),
-          
-          
         ],
       ),
     );
